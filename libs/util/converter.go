522123f7a8cccbd86df45a637a93894f220baf8b
rodericusifo <rodericus1999@gmail.com>
2023-01-21 02:39:04 +0700

    add converter

--- libs/util/converter.go
+++ libs/util/converter.go
@@ -2,6 +2,8 @@ package util
 
 import (
 	"encoding/json"
+	"fmt"
+	"strings"
 )
 
 func ConvertToStruct[T any](obj any) (T, error) {
@@ -15,4 +17,12 @@ func ConvertToStruct[T any](obj any) (T, error) {
 		return *c, err
 	}
 	return *c, nil
+}
+
+func ConvertMapKeysToString[K comparable, V any](m map[K]V) string {
+	keys := make([]string, 0, len(m))
+	for k := range m {
+		keys = append(keys, fmt.Sprint(k))
+	}
+	return "[" + strings.Join(keys, ", ") + "]"
 }
